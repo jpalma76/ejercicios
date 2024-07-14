670798e5dd50023af691200a4e355717844bf786
Juan Palma <juan.palma.alvarez@gmail.com>
2024-07-13 20:50:31 -0400

    agrega ejercicios

--- contador.py
+++ contador.py
@@ -1,5 +1,23 @@
-contador = 1
+'''contador = 1
 while contador <= 10:
     print(contador)
     contador += 1
 print("Fin de ciclo")
+
+print(2**3**2)
+print(2**3)
+print(3**2)
+'''
+nombre = "Juan"
+edad = 47
+print("Hola, soy", nombre, end=" ")
+print("y tengo", edad, "años")
+print("cumplidos")
+print()
+
+print("Este print", end=" :D ")
+print("no cambia", "de linea", end=";)")
+
+print()
+print("-El tesoro", "está", "en", sep="...")
+#print()
